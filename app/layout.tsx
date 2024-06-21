4a56121b888d7fa6ecd9d059d9af3c5b2943865e
Maureno Ammar <maurenoammar4@gmail.com>
2024-06-21 20:38:22 +0700

    UPDATE

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import Nav from "./componet/Nav";
+import Cart from "./componet/Cart";
+import Filter from "./componet/Filter";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} flex justify-center items-center`}>
+        {children}
+        <Cart />
+      </body>
     </html>
   );
 }
